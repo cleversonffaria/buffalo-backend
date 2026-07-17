de14b3b31aeec77ce7a27d7cdeba863a7df45a6d
Cleverson Fernandes <cleversonffaria@gmail.com>
2026-07-17 16:09:24 -0300

    feat: adicionar logging de requisições e tempo de resposta

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { ValidationPipe } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NextFunction, Request, Response } from "express";
 import { HttpExceptionLoggingFilter } from "src/common/filters/http-exception.filter";
 import { AppModule } from "src/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger("Http");
 
   app.enableCors({
     origin: true,
@@ -19,6 +21,17 @@ async function bootstrap() {
     })
   );
   app.useGlobalFilters(new HttpExceptionLoggingFilter());
+  app.use((request: Request, response: Response, next: NextFunction) => {
+    const startedAt = Date.now();
+
+    response.on("finish", () => {
+      logger.log(
+        `${request.method} ${request.originalUrl} -> ${response.statusCode} ${Date.now() - startedAt}ms`
+      );
+    });
+
+    next();
+  });
 
   await app.listen(process.env.PORT || 3001, "0.0.0.0");
 }
